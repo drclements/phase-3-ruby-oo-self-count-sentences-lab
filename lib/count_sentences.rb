69503d54df81d8e71385ef46d65da33a8098d704
Daniel Clements <drclements@liberty.edu>
2022-12-13 16:37:33 -0500

    commit tree 9491d0a289aeccf861814ca2553bc60dad313d42

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,43 @@ require 'pry'
 class String
 
   def sentence?
-    
+    split_string = self.split("")
+    if split_string[-1] == "."
+      true
+    else false
+    end
   end
-
+  
   def question?
-
+    split_string = self.split("")
+    if split_string[-1] == "?"
+      true
+    else false
+    end
   end
-
+  
   def exclamation?
-
+    split_string = self.split("")
+    if split_string[-1] == "!"
+      true
+    else false
+    end
   end
-
+  
   def count_sentences
-
+    split_string = self.split("")
+    count = 0
+    split_string.each.with_index {|c, index| 
+      if c == "." || c == "!" || c == "?"
+        count += 1 
+        if split_string[index + 1] == "." || split_string[index + 1] == "!" || split_string[index + 1] == "?"
+          count -= 1
+        end
+      end
+    } 
+      count    
   end
 end
+
+binding.pry
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
